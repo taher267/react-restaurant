7690efec9ebf058869c5c54fb170596f7e4c43e5
ABU TAHER <abutaher267@gmail.com>
2021-12-10 01:32:00 +0600

    Creating Menu Component

--- src/component/body/Body.js
+++ src/component/body/Body.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Menu from './Menu';
 const Body = () => {
     return (
-        <div>Body</div>
+        <div>
+            <Menu />
+        </div>
     );
 
 };
---
+++ src/data/dishes.js
@@ -0,0 +1,99 @@
+const DISHES = [
+    {
+        id: 0,
+        name: 'User One',
+        image: 'assets/images/0burger.jpg',
+        category: 'meal',
+        label: 'hot',
+        price: '199',
+        discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quisquam itaque doloribus, sunt repellat inventore, deserunt, consectetur nisi iure expedita cumque? Voluptatum incidunt obcaecati beatae modi sint voluptates repellendus illo?',
+        comments: [
+            {
+                id: 0,
+                ratting: 5,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Taher",
+                date: "2021-10-06T17:45:25.554543Z"
+            },
+            {
+                id: 1,
+                ratting: 3,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Belal",
+                date: "2018-10-06T17:45:25.554543Z"
+            },
+            {
+                id: 2,
+                ratting: 4,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Sumon",
+                date: "2018-10-06T17:45:25.554543Z"
+            }
+        ],
+    },
+    {
+        id: 1,
+        name: 'User Two',
+        image: 'assets/images/2fries.jpg',
+        category: 'meal',
+        label: 'worm',
+        price: '120',
+        discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quisquam itaque doloribus, sunt repellat inventore, deserunt, consectetur nisi iure expedita cumque? Voluptatum incidunt obcaecati beatae modi sint voluptates repellendus illo?',
+        comments: [
+            {
+                id: 0,
+                ratting: 2,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Taher",
+                date: "2021-10-06T17:45:25.554543Z"
+            },
+            {
+                id: 1,
+                ratting: 3,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Belal",
+                date: "2018-10-06T17:45:25.554543Z"
+            },
+            {
+                id: 2,
+                ratting: 1,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Sumon",
+                date: "2018-10-06T17:45:25.554543Z"
+            }
+        ],
+    },
+    {
+        id: 2,
+        name: 'User Three',
+        image: 'assets/images/2pizza.jpg',
+        category: 'meal',
+        label: 'cool',
+        price: '120',
+        discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quisquam itaque doloribus, sunt repellat inventore, deserunt, consectetur nisi iure expedita cumque? Voluptatum incidunt obcaecati beatae modi sint voluptates repellendus illo?',
+        comments: [
+            {
+                id: 0,
+                ratting: 2,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Taher",
+                date: "2021-10-06T17:45:25.554543Z"
+            },
+            {
+                id: 1,
+                ratting: 3,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Belal",
+                date: "2018-10-06T17:45:25.554543Z"
+            },
+            {
+                id: 2,
+                ratting: 1,
+                comment: "isquam itaque doloribus, sunt repellat inventore",
+                author: "Sumon",
+                date: "2018-10-06T17:45:25.554543Z"
+            }
+        ]
+    }
+];
+export default DISHES;
